f2fa3ec81deb8aa3bfbce804cfef4b106095dc8f
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-01-20 09:57:20 +0100

    dodanie podstawowych enpointów dla pytań

--- client/src/features/question/questionApiSlice.ts
+++ client/src/features/question/questionApiSlice.ts
@@ -2,9 +2,12 @@ import { apiSlice } from "../../app/api/apiSlice";
 import Question from "../../types/Question";
 
 type AddQuestionBody = { title: string; content: object; tags: string[] };
+type ToggleVoteBody = { questionID: string, vote: "up"| "down"};
 
 export const questionApiSlice = apiSlice.injectEndpoints({
   endpoints: (build) => ({
+
+    // dodawanie nowego pytania
     addQuestion: build.mutation<void, AddQuestionBody>({
       query: (body) => ({
         url: "/question/add",
@@ -12,10 +15,30 @@ export const questionApiSlice = apiSlice.injectEndpoints({
         body,
       }),
     }),
+
+    // wybieranie pytania po tytule
     getQuestionByTitle: build.query<{ question: Question }, { questionTitle: string }>({
       query: ({ questionTitle }) => `/question/by-title/${questionTitle}`,
     }),
-    
+
+    // zmiana głosu
+    toggleQuestionVote: build.mutation<void, ToggleVoteBody>({
+      query: ({ vote, questionID }) => ({
+        url: "/question/toggleVote",
+        method: "PATCH",
+        body
+      })
+    }),
+
+    // zmiana zaznaczonego pytania
+    toggleMarkekQuestion: build.mutation<void, { questionID: string }>({
+      query: (body) => ({
+        url: "/question/toggleMarked",
+        method: "POST",
+        body
+      })
+    })
+
   }),
 });
 
