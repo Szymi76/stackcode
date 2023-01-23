1c7cbc7cd6683e61d074be9b3fb427a67b16a570
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-01-23 14:09:37 +0100

    .

--- server/config/cookieOptions.js
+++ server/config/cookieOptions.js
@@ -3,7 +3,6 @@ const cookieOptions = {
   sameSite: process.env.NODE_ENV == "dev" ? "Lax" : "None",
   secure: process.env.NODE_ENV == "dev" ? false : true,
   expires: new Date(+new Date() + 1000 * 60 * 60 * 24 * 30 * 3),
-  domain: "api-stackcode.onrender.com",
 };
 
 export default cookieOptions;
