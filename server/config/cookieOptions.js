50bc3669a79104a6d903333161875547d317f68b
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-01-23 13:36:02 +0100

    .

--- server/config/cookieOptions.js
+++ server/config/cookieOptions.js
@@ -2,6 +2,7 @@ const cookieOptions = {
   httpOnly: process.env.NODE_ENV == "dev" ? false : true,
   sameSite: process.env.NODE_ENV == "dev" ? "Lax" : "None",
   secure: process.env.NODE_ENV == "dev" ? false : true,
+  expires: +new Date() + 1000 * 60 * 60 * 24 * 30 * 3,
 };
 
 export default cookieOptions;
