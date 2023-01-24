4d4f77b1498947e62b43b07350c904693aec17e6
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-01-24 20:04:35 +0100

    .

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -11,7 +11,7 @@ const corsOptions = {
   credentials: true,
   // exposedHeaders: [],
   // methods: [],
-  origin: [process.env.DEV_CLIENT_URL, process.env.PROD_CLIENT_URL, "https://stackcode.win"],
+  origin: [process.env.DEV_CLIENT_URL, process.env.PROD_CLIENT_URL, "https://app.stackcode.win"],
 };
 
 export default corsOptions;
