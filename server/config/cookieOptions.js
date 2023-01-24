5f3df1ae59967dc1851651bf2ce363ce9bc6e5c2
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-01-24 19:51:23 +0100

    .

--- server/config/cookieOptions.js
+++ server/config/cookieOptions.js
@@ -3,7 +3,7 @@ const cookieOptions = {
   sameSite: process.env.NODE_ENV == "dev" ? "Lax" : "None",
   secure: process.env.NODE_ENV == "dev" ? false : true,
   expires: new Date(+new Date() + 1000 * 60 * 60 * 24 * 30 * 3),
-  domain: ["stackcode.win", ".stackcode.win"],
+  domain: "stackcode.win",
 };
 
 export default cookieOptions;
