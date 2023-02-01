ffc365a4f5b2d72691725aa43c33763522de4ea6
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-02-01 21:42:27 +0100

    .

--- server/utils/formatError.js
+++ server/utils/formatError.js
@@ -2,7 +2,7 @@ const formatError = ({ name, message, stack }) => {
   try {
     // jeśli serwer jest w produkcji
     const NODE_ENV = process.env.NODE_ENV;
-    if (NODE_ENV != "dev") return { name: "Error", message: "Something went wrong", files: [] };
+    // if (NODE_ENV != "dev") return { name: "Error", message: "Something went wrong", files: [] };
 
     // tablica plików z błędami
     const pattern = /\((.*?)\)/g;
