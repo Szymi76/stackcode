0299b4b6698efb6566fb6d4755454c7b4735ad2f
Szymi76 <87788260+Szymi76@users.noreply.github.com>
2023-02-15 12:44:01 +0100

    Lekkie poprawki

--- client/src/security/AccessForAll.tsx
+++ client/src/security/AccessForAll.tsx
@@ -2,8 +2,14 @@ import { Outlet } from "react-router";
 // import Nav from "../components/Nav";
 import Nav from "../components/Nav/index";
 import Footer from "../components/Footer";
+import React, { useEffect } from "react";
+import { useLocation, Navigate } from "react-router-dom";
 
 const AccessForAll = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => localStorage.setItem("last-pathname", pathname), []);
+
   return (
     <>
       <Nav />
